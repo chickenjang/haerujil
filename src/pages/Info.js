ae065d2e3378cbcd1aad67f9b2ba8964d5f563ac
jiwon <scorpiusjwj@gmail.com>
2022-03-17 09:19:48 +0900

    [feat] 로그인 상태에서만 내정보 페이지 보이도록 구현

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,4 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 function Info() {
+  const selector = useSelector((state) => state.login);
+  if (!selector) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
   return <div>내정보</div>;
 }
 
